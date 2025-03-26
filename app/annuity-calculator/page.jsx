8d0d67c5a4a0c8334dcfe552ae7a73c66c02f9d1
Mubashir1920 <131754637+Mubashir1920@users.noreply.github.com>
2025-03-26 19:46:08 +0500

    Changes for Merge Of Ord Anu

--- app/annuity-calculator/page.jsx
+++ app/annuity-calculator/page.jsx
@@ -17,13 +17,6 @@ export const metadata = {
 
 export default function Home() {
     return (
-        <main className="container grid grid-cols-1 lg:grid-cols-2 gap-2 mx-auto py-20 px-4">
-            <div className="max-w-2xl mx-auto">
-                <OrdinaryAnnuity />
-            </div>
-            <div className="max-w-2xl mx-auto">
-                <AnnuityDueCalculator />
-            </div>
-        </main>
+        <div>Hello</div>
     )
 }
